0b44ffe7bc8257eb2eee1c0aec85bef9eaf0b98e
jmcculloch2018 <jmcculloch2018@francisparker.org>
2015-08-03 20:04:25 -0700

    Initial Commit

---
+++ iAdAndGLKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iAdAndGLKit
+//
+//  Created by Jeremy McCulloch on 8/3/15.
+//  Copyright © 2015 Jeremy McCulloch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iAdAndGLKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iAdAndGLKit
+//
+//  Created by Jeremy McCulloch on 8/3/15.
+//  Copyright © 2015 Jeremy McCulloch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
